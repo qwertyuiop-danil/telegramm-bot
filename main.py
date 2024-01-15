a138b5110b5a693f0f3610eee8116249e6cdd053
Danil <88977206+qwertyuiop-danil@users.noreply.github.com>
2024-01-15 18:23:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import telebot;token='6421982853:AAGpLTpCiV4byFyGHDoelPj-PwQyGca6TvA';bot = telebot.TeleBot(token);bl = [];bot.send_message(1346243726, 'Start')
+import telebot;token='6421982853:AAGpLTpCiV4byFyGHDoelPj-PwQyGca6TvA';bot = telebot.TeleBot(token);wl = [1346243726];bot.send_message(wl[0], 'Start')
 @bot.message_handler()
 def f(m):
     try:
-        if('eval:' in m.text and not m.chat.id in bl)or('eval:' in m.text and m.chat.id==1346243726):bot.send_message(m.chat.id, eval(''.join(m.text.split('eval:')[1:]))) 
-        if('exec:' in m.text and not m.chat.id in bl)or('exec:' in m.text and m.chat.id==1346243726):exec(''.join(m.text.split('exec:')[1:]));bot.send_message(m.chat.id, 'S')
-    except:bot.send_message(m.chat.id, 'E')
+        if('exec:' in m.text and m.chat.id in wl):exec(''.join(m.text.split('exec:')[1:]));bot.send_message(m.chat.id, 'S')
+        elif m.chat.id in wl:r=eval(m.text);bot.send_message(m.chat.id, '\n'.join([str(x) for x in r]) if (str(type(r))=="<class 'list'>" or str(type(r))=="<class 'dict'>") else (r if r!=None else 'S'))
+    except Exception as e:bot.send_message(m.chat.id, e)
 bot.polling(none_stop=True)
